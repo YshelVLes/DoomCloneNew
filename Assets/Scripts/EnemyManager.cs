0a0684d145038ca36e7a795edf7ab339c0a4abd5
DreameaterBane <102740415+DreameaterBane@users.noreply.github.com>
2023-12-18 12:42:28 +0300

    Deleted some garbage

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 
 public static class EnemyManager 
 {
-    //статическое поле - работает всю жизнь программы, его можно достать изо всех точек кода 
-    //HashSet - все эл уликальные 
     static HashSet<DamagableComponent> damagableComponents = new HashSet<DamagableComponent>();
 
    public static DamagableComponent GetFirstVisibleTarget(
@@ -18,10 +16,8 @@ public static class EnemyManager
 
         foreach (DamagableComponent enemy in EnemyManager.Enemies.Where(damagable => (damagable.Affiliation & affiliation) > 0))
         {
-            // transform.forward
             Vector3 enemyDirection = enemy.transform.position - sourceTransform.position;
 
-            //если длина вектора больше чем            - пропускаем этот элемент в foreach 
             if (enemyDirection.sqrMagnitude > maxDistance * maxDistance)
                 continue;
 
@@ -32,10 +28,6 @@ public static class EnemyManager
 
             enemyDirection = enemyDirection.normalized;
 
-            // Product - это умножение
-            //—кал€рное и векторное умножение 
-            // Dot Product - разница между двум€ векторами, скал€рное умножение 
-
             float angle = Mathf.Acos(Vector3.Dot(sourceTransform.forward, enemyDirection2D)) * Mathf.Rad2Deg;
 
             if (angle < coneAngle)
@@ -43,8 +35,6 @@ public static class EnemyManager
                 CharacterController enemyCollider = enemy.GetComponent<CharacterController>();
 
                 Vector3 unitFrac = new Vector3(0, enemyCollider.height / 2);
-
-                // RaycastHit hit; 
 
                 if (AimLineAttack(sourceTransform, enemy.transform.position)
                     || AimLineAttack(sourceTransform, enemy.transform.position + unitFrac)
@@ -56,22 +46,10 @@ public static class EnemyManager
 
             }
 
-            //if (angle < 3)
-            //{
-            //    aim.CanShoot = true;
-            //    print("CanShoot");
-            //}
-            //else return;
-
         }
 
         return null;
-
-
-        //if(  Physics.Raycast(transform.position, transform.forward, out RaycastHit hit) && hit.collider.TryGetComponent(out DamagableComponent damagable) )
-        //  {
-        //      Debug.Log("Can Damage");
-        //  }
+       
     }
     
 
@@ -86,7 +64,6 @@ public static class EnemyManager
         return false;
     }
 
-    //ћожно IEnumerator - это объекты которые можно перечисл€ть 
     public static IReadOnlyCollection<DamagableComponent> Enemies => damagableComponents;
 
     public static void RegisterEnemy(DamagableComponent damagable)
